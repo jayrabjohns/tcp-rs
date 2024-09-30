91cae07ab73761488e8a4f799692c67d18c15441
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-09-30 21:39:58 +0100

    chore: generalise write and segment check

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,5 +1,10 @@
-use std::{cmp::Ordering, io, net::Ipv4Addr};
+use std::{
+    cmp::Ordering,
+    io::{self, Write},
+    net::Ipv4Addr,
+};
 
+use anyhow::Result;
 use etherparse::{IpNumber, Ipv4Header, Ipv4HeaderSlice, TcpHeader, TcpHeaderSlice};
 use tun_tap::Iface;
 
@@ -74,15 +79,16 @@ pub struct Tcb {
     recv: RecvSequenceVariables,
     send: SendSequenceVariables,
     send_ip_header: Ipv4Header,
+    send_tcp_header: TcpHeader,
 }
 
 impl Tcb {
     pub fn accept_connection(
-        nic: &Iface,
+        mut nic: &Iface,
         ip_header: Ipv4HeaderSlice,
         tcp_header: TcpHeaderSlice,
         data: &[u8],
-    ) -> io::Result<Option<Self>> {
+    ) -> Result<Option<Self>> {
         println!(
             "{} -> {}:{} {}b of TCP",
             ip_header.source_addr(),
@@ -97,25 +103,26 @@ impl Tcb {
         }
 
         let iss = 0;
+        let wnd = 10;
 
         let recv = RecvSequenceVariables {
-                irs: tcp_header.sequence_number(),
-                nxt: tcp_header.sequence_number() + 1,
-                wnd: tcp_header.window_size(),
-                up: false,
+            irs: tcp_header.sequence_number(),
+            nxt: tcp_header.sequence_number() + 1,
+            wnd: tcp_header.window_size(),
+            up: false,
         };
 
         let send = SendSequenceVariables {
-                iss,
-                una: iss + 1,
-                nxt: iss + 1,
-                wnd: 10,
-                up: false,
-                wl1: 0,
-                wl2: 0,
+            iss,
+            una: iss,
+            nxt: iss,
+            wnd,
+            up: false,
+            wl1: 0,
+            wl2: 0,
         };
 
-        let syn_ack_tcp_header = TcpHeader {
+        let send_tcp_header = TcpHeader {
             source_port: tcp_header.destination_port(),
             destination_port: tcp_header.source_port(),
             acknowledgment_number: recv.nxt,
@@ -126,7 +133,7 @@ impl Tcb {
             ..Default::default()
         };
 
-        let send_ip_header_payload_len: u16 = syn_ack_tcp_header.header_len_u16();
+        let send_ip_header_payload_len: u16 = send_tcp_header.header_len_u16();
         let send_ip_header_ttl: u8 = 64;
         let send_ip_header_protocol: IpNumber = IpNumber::TCP;
         let send_ip_header_source: [u8; 4] = ip_header.destination();
@@ -144,31 +151,15 @@ impl Tcb {
         println!("Received ip header: \n{:02x?}", ip_header.slice());
         println!("Received tcp header: \n{:02x?}", tcp_header.slice());
 
-        let mut buf: [u8; ETH_MTU] = [0; ETH_MTU];
-        let num_written_bytes: usize = {
-            let buf_len: usize = buf.len();
-
-            let mut unwritten_bytes: &mut [u8] = &mut buf[..];
-
-            send_ip_header.write(&mut unwritten_bytes)?;
-
-            syn_ack_tcp_header.write(&mut unwritten_bytes)?;
-
-            buf_len - unwritten_bytes.len()
-        };
-
-        let response: &[u8] = &buf[..num_written_bytes];
-
-        println!("Response ({num_written_bytes}b): \n{:02x?}", response);
-
-        nic.send(response)?;
-
-        let tcb = Tcb {
+        let mut tcb = Tcb {
             state: State::SynRcvd,
             send,
             recv,
             send_ip_header,
+            send_tcp_header,
         };
+
+        tcb.write(nic, &[])?;
 
         return Ok(Some(tcb));
     }
@@ -179,21 +170,205 @@ impl Tcb {
         ip_header: Ipv4HeaderSlice,
         tcp_header: TcpHeaderSlice,
         data: &[u8],
-    ) -> io::Result<()> {
+    ) -> Result<()> {
+        // Check ack is valid. una < ack <= nxt (but with wrapping arithmatic)
+        if !is_between_values_wrapped(
+            tcp_header.acknowledgment_number(),
+            self.send.una,
+            self.send.nxt.wrapping_add(1),
+        ) {
+            if !self.state.is_synchronised() {
+                self.send_tcp_header.sequence_number = tcp_header.acknowledgment_number();
+                self.send_rst(nic)?;
+            }
+            return Ok(());
+        }
+
+        if !self.is_segment_valid(&tcp_header, data) {
+            return Ok(());
+        }
+
+        match self.state {
+            State::SynRcvd => {
+                if !tcp_header.ack() {
+                    return Ok(());
+                }
+
+                self.state = State::Estab;
+                self.send_tcp_header.fin = true; //TODO: store in retransmission queue
+                self.write(nic, &[])?;
+                self.state = State::FinWait;
+            }
+            State::Estab => {
+                if !tcp_header.fin() || !data.is_empty() {
+                    todo!()
+                }
+
+                self.write(nic, &[])?;
+                self.state = State::CloseWait;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// RFC 793 Section 3.3
+    /// The first part of this test checks to see if the beginning of the
+    /// segment falls in the window, the second part of the test checks to see
+    /// if the end of the segment falls in the window; if the segment passes
+    /// either part of the test it contains data in the window.
+    ///
+    /// Actually, it is a little more complicated than this.  Due to zero
+    /// windows and zero length segments, we have four cases for the
+    /// acceptability of an incoming segment:
+    ///```
+    ///   Segment Receive  Test
+    ///   Length  Window
+    ///   ------- -------  -------------------------------------------
+    ///
+    ///      0       0     SEG.SEQ = RCV.NXT
+    ///
+    ///      0      >0     RCV.NXT =< SEG.SEQ < RCV.NXT+RCV.WND
+    ///
+    ///     >0       0     not acceptable
+    ///
+    ///     >0      >0     RCV.NXT =< SEG.SEQ < RCV.NXT+RCV.WND
+    ///                 or RCV.NXT =< SEG.SEQ+SEG.LEN-1 < RCV.NXT+RCV.WND
+    /// ```
+    fn is_segment_valid(&self, tcp_header: &TcpHeaderSlice, data: &[u8]) -> bool {
+        let seqn = tcp_header.sequence_number();
+        let seg_len: u32 = {
+            let mut slen = data.len();
+            if tcp_header.fin() {
+                slen += 1;
+            }
+
+            if tcp_header.syn() {
+                slen += 1;
+            }
+            slen as u32
+        };
+        let window = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
+
+        if seg_len == 0 {
+            if self.recv.wnd == 0 {
+                seqn == self.recv.nxt
+            } else {
+                is_between_values_wrapped(seqn, self.recv.nxt.wrapping_sub(1), window)
+            }
+        } else {
+            if self.recv.wnd == 0 {
+                false
+            } else {
+                is_between_values_wrapped(seqn, self.recv.nxt.wrapping_sub(1), window)
+                    || is_between_values_wrapped(
+                        seqn + seg_len - 1,
+                        self.recv.nxt.wrapping_sub(1),
+                        window,
+                    )
+            }
+        }
+    }
+
+    fn write(&mut self, nic: &Iface, payload: &[u8]) -> Result<usize> {
+        let mut buf: [u8; ETH_MTU] = [0; ETH_MTU];
+
+        self.send_tcp_header.sequence_number = self.send.nxt;
+        self.send_tcp_header.acknowledgment_number = self.recv.nxt;
+
+        let size = std::cmp::min(
+            buf.len(),
+            self.send_tcp_header.header_len() + self.send_ip_header.header_len() + payload.len(),
+        );
+
+        self.send_ip_header.set_payload_len(size)?;
+
+        self.send_ip_header
+            .set_payload_len(self.send_tcp_header.header_len() + payload.len())?;
+
+        let buf_len: usize = buf.len();
+
+        let mut unwritten_bytes: &mut [u8] = &mut buf[..];
+
+        self.send_ip_header.write(&mut unwritten_bytes)?;
+
+        self.send_tcp_header.write(&mut unwritten_bytes)?;
+
+        let payload_bytes: usize = unwritten_bytes.write(payload)?;
+
+        let num_written_bytes: usize = buf_len - unwritten_bytes.len();
+
+        let response: &[u8] = &buf[..num_written_bytes];
+
+        self.send.nxt = self.send.nxt.wrapping_add(payload_bytes as u32);
+
+        if self.send_tcp_header.syn {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.send_tcp_header.syn = false;
+        }
+
+        if self.send_tcp_header.fin {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.send_tcp_header.fin = false;
+        }
+
+        nic.send(response)?;
+
+        println!("Response ({num_written_bytes}b): \n{:02x?}", response);
+
+        Ok(payload_bytes)
+    }
+
+    fn send_rst(&mut self, nic: &Iface) -> Result<()> {
+        // TODO fix sequence numbers
+        self.send_tcp_header.rst = true;
+        self.send_tcp_header.sequence_number = 0;
+        self.send_tcp_header.acknowledgment_number = 0;
+        self.write(nic, &[])?;
+
         Ok(())
     }
 }
 
-#[derive(Clone, Copy)]
-pub enum State {
-    Closed,
-    Listen,
-    SynRcvd,
-    // Estab,
+/// lower < value < upper
+/// but with wrapping arithmatic
+/// TODO: without branching
+fn is_between_values_wrapped(value: u32, start: u32, end: u32) -> bool {
+    match start.cmp(&value) {
+        Ordering::Equal => return false,
+        Ordering::Less => {
+            if end >= start && end <= value {
+                return false;
+            }
+        }
+        Ordering::Greater => {
+            if end > value && end < start {
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
-impl Default for State {
-    fn default() -> Self {
-        Self::Closed
+#[derive(Clone, Copy)]
+pub enum State {
+    // Closed,
+    // Listen,
+    SynRcvd,
+    Estab,
+    FinWait,
+    CloseWait,
+}
+
+impl State {
+    pub fn is_synchronised(&self) -> bool {
+        use State::*;
+
+        match self {
+            SynRcvd => false,
+            Estab | FinWait | CloseWait => true,
+        }
     }
 }
